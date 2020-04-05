21c4d01671fb55befc9444db79978e5db41b24a5
nam268ek <namnguyenexe@gmail.com>
2020-04-06 00:28:08 +0700

    Search value input

--- ExpressJS_practice/index.js
+++ ExpressJS_practice/index.js
@@ -2,16 +2,36 @@ const express = require("express");
 const app = express();
 const port = 3456;
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
+var users = [
+    { id: 1, name: "NAM" },
+    { id: 2, name: "SON" },
+  ];
 //Bài 2 - Template engines
-app.get('/', function(req, res) {
-    res.render('index');
+app.get("/", function (req, res) {
+  res.render("index", {
+    name: "Nam",
+  });
 });
-app.get('/user', function(req, res) {
-    res.render('user');
+app.get("/user", function (req, res) {
+  res.render("./user/user", {
+    users: users
+  });
 });
-app.listen(3456, function() {
-    console.log("Server listening on port" + port);
+
+app.get('/user/search', function(req, res) {
+    var query = req.query.value;
+    var newArrUsers = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    
+    res.render('user/user',{
+        users: newArrUsers
+    });
+});
+
+app.listen(3456, function () {
+  console.log("Server listening on port" + port);
 });
