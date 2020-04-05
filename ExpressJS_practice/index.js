cf8a9f8e390b57883b07610cba439f94a604fdd5
nam268ek <namnguyenexe@gmail.com>
2020-04-05 14:54:46 +0700

    Using template engines

--- ExpressJS_practice/index.js
+++ ExpressJS_practice/index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3456;
 
-app.get('/', function(req, res) {
-    res.send("Well comeback");
-});
+app.set('view engine', 'pug');
+app.set('views', './views');
 
+//Bài 2 - Template engines
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.get('/user', function(req, res) {
+    res.render('user');
+});
 app.listen(3456, function() {
     console.log("Server listening on port" + port);
 });
