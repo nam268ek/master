58e497f539382a1a8ee1f47804a05728ab6ba94b
nam268ek <namnguyenexe@gmail.com>
2020-04-07 16:17:36 +0700

    Lam quen vs POST

--- ExpressJS_practice/index.js
+++ ExpressJS_practice/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3456;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -15,6 +18,7 @@ app.get("/", function (req, res) {
     name: "Nam",
   });
 });
+
 app.get("/user", function (req, res) {
   res.render("./user/user", {
     users: users
@@ -32,6 +36,14 @@ app.get("/user/search", function (req, res) {
     });
 });
 
+app.get("/user/create",function (req, res) {
+  res.render('user/create');
+});
+
+app.post("/user/create", function (req, res) {
+  users.push(req.body);
+  res.redirect("/user");
+});
 app.listen(3456, function () {
-  console.log("Server listening on port " + port);
+  console.log("Server listening on port ");
 });
