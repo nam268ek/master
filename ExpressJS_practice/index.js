e417aa530a8831a78b7cc9f28f9ad0c6f0d923a8
nam268ek <namnguyenexe@gmail.com>
2020-04-11 21:23:34 +0700

    SESSION_SECRET Value

--- ExpressJS_practice/index.js
+++ ExpressJS_practice/index.js
@@ -1,16 +1,18 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
-var userRouter = require('./routes/user.route');
-var authRouter = require('./routes/auth.route');
-var authMiddleware = require('./middleware/auth.middleware');
+var userRouter = require("./routes/user.route");
+var authRouter = require("./routes/auth.route");
+var authMiddleware = require("./middleware/auth.middleware");
+var cookieParser = require("cookie-parser");
 
-//! Làm ơn nhớ require cookie
-var cookieParser = require('cookie-parser');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -21,8 +23,8 @@ app.get("/", function (req, res) {
   });
 });
 
-app.use('/user', authMiddleware.requireAuth, userRouter);
-app.use('/auth', authRouter);
+app.use("/user", authMiddleware.requireAuth, userRouter);
+app.use("/auth", authRouter);
 
 app.listen(3456, function () {
   console.log("Server running... ");
