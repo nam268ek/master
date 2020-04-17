53d2a50a067f625e801d0183c2092d1805fc15fc
nam268ek <namnguyenexe@gmail.com>
2020-04-17 21:28:00 +0700

    switch to mongoose

--- ExpressJS_practice/index.js
+++ ExpressJS_practice/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
@@ -10,6 +10,9 @@ var authMiddleware = require("./middleware/auth.middleware");
 var sessionMiddleware = require("./middleware/session.middleware");
 var cookieParser = require("cookie-parser");
 var cartRouter = require("./routes/cart.route");
+var apiproductRouter = require('./api/routes/products.route');
+var db = mongoose.connection;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,8 +20,19 @@ app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(sessionMiddleware.sessionId);
 app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGO_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.set("view engine", "pug");
 app.set("views", "./views");
+
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log('-----------CONNECTED MONGODB-----------');
+});
 
 app.get("/", function (req, res) {
   res.render("index", {
@@ -30,6 +44,7 @@ app.use("/cart", cartRouter);
 app.use("/products", authMiddleware.requireAuth, productsRouter);
 app.use("/user", authMiddleware.requireAuth, userRouter);
 app.use("/auth", authRouter);
+app.use("/api/products", apiproductRouter);
 
 app.listen(3456, function () {
   console.log("Server running... ");
